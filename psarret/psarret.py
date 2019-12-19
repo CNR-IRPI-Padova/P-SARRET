3851a37234ef76b5661ee496dda2a01ece8a22bb
giacomo titti <giacomotitti@gmail.com>
2019-12-19 12:56:08 +0100

    edit psarret.py

--- psarret/psarret.py
+++ psarret/psarret.py
@@ -15,65 +15,66 @@ class psarretPlugin:
         self.iface = iface
 
     def initGui(self):
-      icon = os.path.join(os.path.join(cmd_folder, 'icon.png'))
-      self.action = QAction(QIcon(icon), 'Generate time-series', self.iface.mainWindow())
-      self.action.triggered.connect(self.run)
-      self.iface.addPluginToMenu('&PSARRET', self.action)
-      self.iface.addToolBarIcon(self.action)
+        icon = os.path.join(os.path.join(cmd_folder, 'icon.png'))
+        self.action = QAction(QIcon(icon), 'Generate time-series', self.iface.mainWindow())
+        self.action.triggered.connect(self.run)
+        self.iface.addPluginToMenu('&PSARRET', self.action)
+        self.iface.addToolBarIcon(self.action)
 
     def unload(self):
-      self.iface.removeToolBarIcon(self.action)
-      self.iface.removePluginMenu('&PSARRET', self.action)
-      del self.action
+        self.iface.removeToolBarIcon(self.action)
+        self.iface.removePluginMenu('&PSARRET', self.action)
+        del self.action
 
     def run(self):
-      self.iface.messageBar().pushMessage('Time-series generated')
-      # Layer structure
-      layer = iface.activeLayer()
-      fields = layer.fields()
-      DATES=[]
-      ys=[]
-      IDs=[]
-      features = layer.selectedFeatures()
-      field_names = [field.name() for field in fields]
-
-      #plot single figure
-      for feat in features:
-          attrs = feat.attributes()
-          datetitle = field_names[6:-1]
-          DATE = [dt.strptime(xk,'%Y%m%d') for xk in datetitle]
-          DATES.append(DATE)
-          #date_strings = [d.strftime('%Y-%m-%d') for d in datetitle]
-          y = attrs[6:-1]#input desired range
-          IDs.append(attrs[0])
-          ys.append(y)
-          x = range(len(y))
-          coef = np.polyfit(x,y,1) #linear fit
-          poly1d_fn = np.poly1d(coef)
-          plt.figure()
-          plt.plot(DATE,y,'-r',DATE,y,'bo',DATE,poly1d_fn(x), '--k')#if date is not desired, plot simply x
-          plt.ylabel('Displacement [cm]')
-          plt.xlabel('Date')
-          plt.xticks(rotation=45)
-          plt.title('ID '+attrs[0]+' Scan series - rate = '+str(coef[0]))
-          plt.grid(b=True, which='major', color='#666666', linestyle='-')
-          plt.minorticks_on()
-          plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
-          plt.show()
-
-      #plot total figure
-      plt.figure()
-      for t in range(len(ys)):
-          plt.plot(DATES[t],ys[t])#if date is not desired, plot simply x
-      plt.legend(IDs)
-      for t in range(len(ys)):
-          plt.plot(DATES[t],ys[t],'bo')#if date is not desired, plot simply x
-      plt.ylabel('Displacement [cm]')
-      plt.xlabel('Date')
-      plt.xticks(rotation=45)
-      #plt.title('Total')
-      plt.grid(b=True, which='major', color='#666666', linestyle='-')
-      plt.minorticks_on()
-      plt.legend(IDs)
-      plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
-      plt.show()
+        # Layer structure
+        layer = iface.activeLayer()
+        fields = layer.fields()
+        DATES=[]
+        ys=[]
+        IDs=[]
+        features = layer.selectedFeatures()
+        field_names = [field.name() for field in fields]
+        if len(features)>0:
+            #plot single figure
+            for feat in features:
+                attrs = feat.attributes()
+                datetitle = field_names[6:-1]
+                DATE = [dt.strptime(xk,'%Y%m%d') for xk in datetitle]
+                DATES.append(DATE)
+                #date_strings = [d.strftime('%Y-%m-%d') for d in datetitle]
+                y = attrs[6:-1]#input desired range
+                IDs.append(attrs[0])
+                ys.append(y)
+                x = range(len(y))
+                coef = np.polyfit(x,y,1) #linear fit
+                poly1d_fn = np.poly1d(coef)
+                plt.figure()
+                plt.plot(DATE,y,'-r',DATE,y,'bo',DATE,poly1d_fn(x), '--k')#if date is not desired, plot simply x
+                plt.ylabel('Displacement [cm]')
+                plt.xlabel('Date')
+                plt.xticks(rotation=45)
+                plt.title('ID '+attrs[0]+' Scan series - rate = '+str(coef[0]))
+                plt.grid(b=True, which='major', color='#666666', linestyle='-')
+                plt.minorticks_on()
+                plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+                plt.show()
+            #plot total figure
+            plt.figure()
+            for t in range(len(ys)):
+                plt.plot(DATES[t],ys[t])#if date is not desired, plot simply x
+            plt.legend(IDs)
+            for t in range(len(ys)):
+                plt.plot(DATES[t],ys[t],'bo')#if date is not desired, plot simply x
+            plt.ylabel('Displacement [cm]')
+            plt.xlabel('Date')
+            plt.xticks(rotation=45)
+            #plt.title('Total')
+            plt.grid(b=True, which='major', color='#666666', linestyle='-')
+            plt.minorticks_on()
+            plt.legend(IDs)
+            plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+            plt.show()
+            self.iface.messageBar().pushMessage('Time-series generated')
+        else:
+            self.iface.messageBar().pushMessage('select almost one feature')
